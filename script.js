66c2e9d245d57c2210932ee48c1df4999b9090fe
Neil <nkurien97@gmail.com>
2020-10-27 01:43:58 +0530

    v1

--- script.js
+++ script.js
@@ -1,29 +1,100 @@
-let myLibrary = [];
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+const isReadInputValues = document.getElementsByName('isRead');
+let isReadInput;
+const libraryTable = document.getElementById('library');
 
-function acceptInput () {
-    //set up and accept input values
-    let name = document.getElementbyID('name');
-    let author = document.getElementbyID('author');
-    let pages = document.getElementbyID('pages');
-    Book(name, author, pages);
-}
+/* console.log(document.getElementsByName('isRead')); */
 
-//accept input from html with AddBooktoLibrary()
-//convert input into compatible format and use Book() prototype to make it
-//add book to myLibrary array
-//create element on DOM to display book
+let myLibrary = [
+    {title : "The Design of Everyday Things", author : "Don Norman", pages : 347, isRead : true},
+    {title : "User Friendly", author : "Cliff Kuang", pages : 243, isRead : false}
+];
 
-function Book(name, author, pages, isRead) {
+//Submit Button event listener
+const submit = document.getElementById('submit');
+submit.addEventListener('click', addBookToLibrary);
+
+function Book(title, author, pages, isRead) {
 // the constructor...
-    this.name = name
+    this.title = title
     this.author = author
     this.pages = pages
     this.isRead = isRead;
 }
 
-const newBook = new Book(name, author, pages, isRead);
-myLibrary += newBook
+//isReadInputValues is an array, checks which is selected and returns only that value to isReadInput  
+function checkIsRead () {
+    for (const rb of isReadInputValues) {
+        if (rb.checked) {
+            isReadInput = rb.value;
+        }
+    }
+    return isReadInput
+}
 
 function addBookToLibrary() {
-
+    checkIsRead ();
+    const newBook = new Book(titleInput.value, authorInput.value, pagesInput.value, isReadInput);
+    myLibrary.push(newBook);
+    updateLibrary();
 }
+
+function updateLibrary() {
+
+    //filter through all array elements
+    myLibrary.map(Book => {
+        const libraryContainer = document.getElementById('library-cards');
+        const libraryCards = document.createElement('div');
+        libraryCards.classList.add("book");
+    
+        const cardTitle = document.createElement('h1');
+        cardTitle.classList.add("title");
+        cardTitle.innerHTML = Book.title;
+        libraryCards.appendChild(cardTitle);
+        
+        const cardAuthor = document.createElement('h3');
+        cardAuthor.classList.add("author");
+        cardAuthor.innerHTML = Book.author;
+        libraryCards.appendChild(cardAuthor);
+        
+        const cardPages = document.createElement('p');
+        cardPages.classList.add("pages");
+        cardPages.innerHTML = Book.pages;
+        libraryCards.appendChild(cardPages);
+        
+        const cardIsRead = document.createElement('button');
+        cardIsRead.setAttribute('id', 'mark-as-read');
+        cardIsRead.addEventListener('onclick', toggleIsRead);
+    
+        //add different classes depending on isRead status
+        if (Book.isRead == "true") {
+            cardIsRead.classList.add("read");
+            cardIsRead.innerHTML = "Finished";
+        }
+        else {
+            //cardIsRead.classList.add("");
+            cardIsRead.innerHTML = "Mark as Read";
+        }
+        libraryCards.appendChild(cardIsRead);
+        libraryContainer.appendChild(libraryCards);
+    });
+}
+
+//event listener function for books mark-as-read button
+function toggleIsRead () {
+    if (this.isRead == "true") {
+        this.isRead = "false";
+        cardIsRead.classList.remove("read");
+        cardIsRead.innerHTML = "Mark as Read";
+    }
+    else if (this.isRead == "false") {
+        this.isRead = "true";
+        cardIsRead.classList.add("read");
+        cardIsRead.innerHTML = "Finished";
+    }
+    console.log("here");
+}
+
+//update library with new books, generated from the array
